7b64dd7aa595086659e87463a88efa0d1d1ad850
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-02-29 11:45:20 +0600

    install swiper js

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Mousewheel, Pagination } from 'swiper/modules';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
